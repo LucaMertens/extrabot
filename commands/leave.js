fcd7d1cf638a408cfabd114cc0ec8b4ff17820ed
Luca Mertens <31664742+LucaMertens@users.noreply.github.com>
2019-01-26 18:12:09 +0100

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -1,6 +1,30 @@
 module.exports = function(message, args, client) {
   
-  try{client.voiceChannel.leave();}
-  catch(err){console.log(err)}
+  switch(args[0]){
+  
+    case "all":
+      if (client.functions.checkAdmin(message.author.id)){
+        client.voiceConnections.forEach(connection => connection.disconnect())
+      }
+      else {
+        message.channel.send("You need to be an admin in order to make the bot leave every server.")
+      }
+
+      break;
+
+    default:
+      let relevantConnection = client.voiceConnections.find((connection) => {
+        return(connection.channel.guild == message.channel.guild)
+      })
+
+      try{
+        if(relevantConnection){
+          relevantConnection.disconnect()
+        }
+      }
+      catch(error){console.log(error)} 
+      break;
+
+  }
   
 };
